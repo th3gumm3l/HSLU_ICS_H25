a77e39fde95c1df8d103273818f97741ff368f7e
th3gumm3l <tim.bernhard@me.com>
2025-10-09 10:58:07 +0200

    Kommentare hinzugefügt und Klassen ergänzt

---
+++ OOP_H25/SW04 - Schnittstellen und Datenkapselung/Uebung/Aufgabe1/JavaDoc/Temperatur.java
@@ -0,0 +1,64 @@
+public class Temperatur
+{
+    private float tempInCelsius;
+    private Element element;
+    
+    public Temperatur()
+    {
+        this.tempInCelsius = 20;
+    }
+
+    public Temperatur(float tempInput)
+    {
+        setTempInCelsius(tempInput);
+    }
+    
+    public Temperatur(float tempInput, Element element) {
+        setTempInCelsius(tempInput);
+        this.element = element;
+    }
+
+    /**
+     * Get-Setter
+     */
+    public float getTempInCelsius(){
+        return tempInCelsius;
+    }
+    
+    public void setTempInCelsius(float tempInput){
+        validateRange(tempInput);
+        tempInCelsius = tempInput;
+    }
+
+    /**
+     * Temp Calculations
+     */
+    public float getTempInKelvin(){
+        return tempInCelsius + 273.15f;
+    }
+
+    public float getTempInFahrenheit(){
+        return ((tempInCelsius * 1.8f)+ 32f);
+    }
+
+    public void addCelsius(float delta) {
+        setTempInCelsius(this.tempInCelsius + delta);
+    }
+
+    private void validateRange(float celsius){
+        if (celsius < -273.15 || celsius > 726.85){
+            System.out.println("Out of Range");
+        }
+    }
+    
+    public String getAggregatZustand(){
+        if (tempInCelsius < element.schmelzpunkt) {
+            return "fest";
+        } else if (tempInCelsius < element.siedepunkt) {
+            return "flüssig";
+        } else {
+            return "gasförmig";
+        }
+    }
+    
+}
---
+++ OOP_H25/SW04 - Schnittstellen und Datenkapselung/Uebung/Aufgabe1/JavaDoc/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"Alle Klassen und Schnittstellen","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Demo"},{"p":"<Unnamed>","l":"Element"},{"p":"<Unnamed>","l":"Point"},{"p":"<Unnamed>","l":"Temperatur"},{"p":"<Unnamed>","l":"TestPoint"}];updateSearchResults();
