2a8f7553cb9b2cca45a95fe46841c9805d59d33b
Tim Bernhard <87070779+th3gumm3l@users.noreply.github.com>
2026-03-03 14:11:56 +0100

    Projekt angepasst

--- Introl_FS25/sw03-cracking_lab/HashAlgo/HashAlgo/Program.cs
+++ Introl_FS25/sw03-cracking_lab/HashAlgo/HashAlgo/Program.cs
@@ -1,36 +1,43 @@
-namespace HashingAlgorithm
+﻿namespace HashingAlgorithm
 {
     class Program
     {
         static void Main(string[] args)
         {
             string[] passwords = { "Haus", "Maus", "Raus", "Laus", "aLus" };
+            string[] salts = { "x9Kp", "Qm3z", "7wLe", "Bv0j", "nR5d" };
 
+            Console.WriteLine("=== Ohne Salt ===");
             foreach (var pw in passwords)
             {
                 string hash = HashingAlgorithm(pw);
                 Console.WriteLine($"Password: {pw} -> Hash: {hash}");
             }
+
+            Console.WriteLine();
+            Console.WriteLine("=== Mit Salt ===");
+            for (int i = 0; i < passwords.Length; i++)
+            {
+                string salted = passwords[i] + salts[i];
+                string hash = HashingAlgorithm(salted);
+                Console.WriteLine($"Password: {passwords[i]} + Salt: {salts[i]} -> Salted Input: {salted} -> Hash: {hash}");
+            }
         }
 
-        static string? HashingAlgorithm(string input)
+        static string HashingAlgorithm(string input)
         {
-            if (input.Length != 4)
-            {
-                return null;
-            }
-
             // Primzahlen pro Position sorgen dafür, dass die Zeichenposition
             // den Hash beeinflusst ("Laus" != "aLus")
-            int[] positionPrimes = { 7, 31, 127, 211 };
+            int[] positionPrimes = { 7, 31, 127, 211, 53, 97, 151, 241 };
 
             long hash = 0;
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < input.Length; i++)
             {
                 int charValue = input[i];
+                int prime = positionPrimes[i % positionPrimes.Length];
                 // Positionsabhängige Berechnung mit Primzahlen und Potenzierung
-                hash += charValue * positionPrimes[i] * (i + 1);
+                hash += charValue * prime * (i + 1);
                 hash *= 31;
                 hash += charValue * charValue + i * 997;
             }
