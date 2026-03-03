6f6afaad21b6b08bedd70a5c4845715d34924e27
Tim Bernhard <87070779+th3gumm3l@users.noreply.github.com>
2026-03-03 15:54:56 +0100

    schmaler gemacht

--- Introl_FS25/sw03-cracking_lab/HashAlgo/HashAlgo/Program.cs
+++ Introl_FS25/sw03-cracking_lab/HashAlgo/HashAlgo/Program.cs
@@ -26,28 +26,18 @@
 
         static string HashingAlgorithm(string input)
         {
-            // Primzahlen pro Position sorgen dafür, dass die Zeichenposition
-            // den Hash beeinflusst ("Laus" != "aLus")
             int[] positionPrimes = { 7, 31, 127, 211, 53, 97, 151, 241 };
-
             long hash = 0;
-
             for (int i = 0; i < input.Length; i++)
             {
                 int charValue = input[i];
                 int prime = positionPrimes[i % positionPrimes.Length];
-                // Positionsabhängige Berechnung mit Primzahlen und Potenzierung
                 hash += charValue * prime * (i + 1);
                 hash *= 31;
                 hash += charValue * charValue + i * 997;
             }
-
-            // Sicherstellen, dass der Wert positiv ist
             hash = Math.Abs(hash);
-
-            // Auf 8 Stellen bringen (10000000 bis 99999999)
             hash = hash % 90000000 + 10000000;
-
             return hash.ToString("D8");
         }
     }
